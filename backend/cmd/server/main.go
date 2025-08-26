ca43ce97bc1fc11e44279af494523f300ff7fdb8
MindlessMuse666 <mindlessmuse.666@gmail.com>
2025-08-27 00:25:13 +0300

    BE: подключена swagger документация

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	httpSwagger "github.com/swaggo/http-swagger"
 
 	"jp-song-trainer/internal/handlers"
 )
@@ -15,11 +16,24 @@ func main() {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
-	// Маршруты
-	r.Post("/api/translate", handlers.Translate)
-	r.Get("/api/history", handlers.GetHistory)
-	r.Delete("/api/history/{id}", handlers.DeleteHistory)
-	r.Put("/api/comments/{id}", handlers.UpdateComment)
+	// API-эндпоинты
+	r.Route("/api/v1", func(v1 chi.Router) {
+		v1.Post("/translate", handlers.Translate)
+		v1.Get("/history", handlers.GetHistory)
+		v1.Delete("/history/{id}", handlers.DeleteHistory)
+		v1.Put("/comments/{id}", handlers.UpdateComment)
+	})
+
+	// Статическое подключение Swagger
+	r.Get("/swagger.yaml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./docs/swagger.yaml")
+	})
+
+	// Swagger UI
+	// Доступно по адресу: http://localhost:8080/swagger/index.html
+	r.Get("/swagger/*", httpSwagger.Handler(
+		httpSwagger.URL("/swagger.yaml"),
+	))
 
 	log.Println("😜 Backend running on :8080")
 	http.ListenAndServe(":8080", r)
